3b1553797679c007847a386501eee6384dad1825
Axel <kakarot.joel@gmail.com>
2022-06-23 14:38:31 +0530

    add storybook and chakra ui

---
+++ app/.storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@chakra-ui/storybook-addon",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true,
+    "emotionAlias": false,
+  },
+}
---
+++ app/helpers/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+    brand:
+    {
+        50: '#ffe5eb',
+        100: '#f9bbc5',
+        200: '#ef91a1',
+        300: '#e7657b',
+        400: '#de3a55',
+        500: '#c5213c',
+        600: '#9a172e',
+        700: '#6f0f20',
+        800: '#450613',
+        900: '#1e0005',
+    }
+}
+
+export default extendTheme({ colors })
